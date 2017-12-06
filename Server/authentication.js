91cee41ad21dbae464eb3b8147436401e3b0a820
Camilo <cadazab@gmail.com>
2017-12-06 14:40:53 +0100

    first commit

---
+++ Server/authentication.js
@@ -0,0 +1,20 @@
+module.exports = {
+  Acct : {
+    user: 'ibs_sup',
+    password: 'pantaitw',
+    server: '192.168.101.162\\acct', // pant-sql-dev
+    database: 'pantaenius'
+  },
+   Test : {
+    user: 'ibs_sup',
+    password: 'pantaitw',
+    server: '192.168.101.162\\test', // pant-sql-dev
+    database: 'pantaenius'
+  },
+   Prod : {
+    user: 'ibs_sup',
+    password: 'pantaitw',
+    server: '192.168.101.169\\ibs', // pant-sql
+    database: 'pantaenius'
+  }
+}
